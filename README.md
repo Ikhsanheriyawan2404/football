25248853f1b19e16cfd0645ba6b9a572ea7c8da9
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2023-03-17 22:59:52 +0700

    Finishingg

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+## How To Install
+```bash
+git clone
+cd football-main
+composer install
+copy .env.example .env
+php artisan key:generate
+php artisan serve
+```
+
+## Visual
+![Screenshot](public/imageg/image1.png)
+![Screenshot](public/imageg/image2.png)
+![Screenshot](public/imageg/image3.png)
+![Screenshot](public/imageg/image4.png)
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
 <p align="center">
--- app/Http/Controllers/StandingController.php
+++ app/Http/Controllers/StandingController.php
@@ -10,11 +10,15 @@ class StandingController extends Controller
     public function index()
     {
         if (request()->ajax()) {
-            $standing = Standing::with('club')->get();
+            $standing = Standing::with('club')
+                ->orderBy('points', 'desc')
+                ->orderBy('goals_for', 'desc')
+                ->get();
+
             return DataTables::of($standing)
                 ->addIndexColumn()
-                ->editColumn('selisih_goal', function (Standing $standing) {
-                    return $standing->goals_for - $standing->goals_againts;
+                ->addColumn('selisih_goal', function (Standing $standing) {
+                    return $standing->goals_for - $standing->goals_against;
                 })
                 ->editColumn('plays', function (Standing $standing) {
                     return $standing->awayGames->count() + $standing->homeGames->count();
